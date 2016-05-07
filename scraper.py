473b051ab3eac8210bac54dd3cd9abc2bb0e055a
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2016-05-07 11:20:55 +0100

    got the main scraper working

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 import tweepy
 import keys
 import mysql.connector as mariadb
+import time
 
 mariadb_connection = mariadb.connect(user=keys.dbuser, password=keys.dbpass, database=keys.database)
 cursor2 = mariadb_connection.cursor(buffered=True) #buffer select query or you'll get unread erros
@@ -27,17 +28,17 @@ def limit_handled(cursor):
         except tweepy.RateLimitError:
             time.sleep(15 * 60)
 
-cursor2.execute("SELECT twithandle, userkey, since_id, max_id FROM foamites WHERE started=1 ORDER BY lastscraped ASC LIMIT 3") ##select 100 people who have gone through the start script
+numtweets = 0
+cursor2.execute("SELECT twithandle, userkey, since_id, max_id FROM foamites WHERE started=1 ORDER BY lastscraped ASC LIMIT 100") ##select 100 people who have gone through the start script
 for row in cursor2:
     twitacct = row[0]
     dbid = row[1]
     sinceid = row[2]
     maxid = row[3]
 
-    for status in tweepy.Cursor(api.user_timeline, id=twitacct, max_id=maxid, since_id=sinceid).items(2): # get a thousand tweets
+    for status in tweepy.Cursor(api.user_timeline, id=twitacct, max_id=maxid).items(1000): # get a thousand tweets
         tweet = status.text
         tweet = tweet.replace('\n','') #strip out all the line breaks that make text file horrible
-        print tweet
 
         encodedtweet = unicode(tweet).encode('utf-8') #encode tweet so it writes to the txt file
         file.write(encodedtweet)
@@ -45,17 +46,22 @@ for row in cursor2:
 
         tweetID = status.id
 
-        if tweetID > sinceid:
+        if tweetID > sinceid: #probably don't need this bit as since_id seems to be broken
             sinceid = tweetID
-            print "new since ID"
+
         elif tweetID < maxid:
             maxid = tweetID - 1
-            print "new max id"
+
     #update with since_id, max_id and started
-    try:
-        cursor3.execute("UPDATE foamites SET since_id = %s, max_id = %s, started = 1 WHERE userkey = %s ", (sinceid, maxid, dbid))
-    except mariadb.Error as error:
-        print("Error: {}".format(error))
+
+
+        try:
+            cursor3.execute("UPDATE foamites SET since_id = %s, max_id = %s, started = 1, lastscraped = CURRENT_TIMESTAMP WHERE userkey = %s ", (sinceid, maxid, dbid,))
+        except mariadb.Error as error:
+            print("Error: {}".format(error))
+        global numtweets
+        numtweets += 1
+        print numtweets
 
 file.close()
 mariadb_connection.commit()
