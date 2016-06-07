5e6899391cb567e96db22778daea0210ba9eece8
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-06-07 20:45:33 +0100

    changed leys.py

--- Scraper/keys.py
+++ Scraper/keys.py
@@ -2,10 +2,15 @@ import os
 
 
 
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
-access_token = os.environ.get("ACCESS_TOKEN")
-access_token_secret = os.environ.get("ACCESS_SECRET")
+#consumer_key = os.environ.get("CONSUMER_KEY")
+#consumer_secret = os.environ.get("CONSUMER_SECRET")
+#access_token = os.environ.get("ACCESS_TOKEN")
+#access_token_secret = os.environ.get("ACCESS_SECRET")
+
+consumer_key = test
+consumer_secret = test
+access_token = test
+access_token_secret = test
 
 print consumer_key
 print consumer_secret
