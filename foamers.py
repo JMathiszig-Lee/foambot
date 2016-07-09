031d8a32f8a186262a36f485791d870b98e405e8
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 13:19:37 +0100

    ar

--- foamers.py
+++ foamers.py
@@ -7,8 +7,11 @@ c = conn.cursor()
 myfile = open('foamers.csv', 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
-c.execute("SELECT twithandle, started FROM foamites ") ##select 100 people who haven't yet been scraped
+c.execute("SELECT twithandle, started FROM foamites LIMIT 10") ##select 100 people who haven't yet been scraped
 for row in c:
     twithandle = row[0]
     started = row[1]
+
+    print twithandle
+    print started
     wr.writerow(twithandle, started)
