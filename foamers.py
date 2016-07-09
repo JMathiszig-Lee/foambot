f280da1cf86e8154d498a90bcdc4ba86120d2544
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 13:37:46 +0100

    fixed db name

--- foamers.py
+++ foamers.py
@@ -1,16 +1,17 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('Scraper/foambot.sqlite')
-cursor2 = conn.cursor()
+conn = sqlite3.connect('foambot.sqlite')
+c = conn.cursor()
 
-#myfile = open('foamers.csv', 'wb')
-#wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-cursor2.execute("SELECT twithandle, userkey FROM foamites WHERE started=0 ORDER BY lastscraped ASC LIMIT 10") ##select 100 people who haven't yet been scraped
-for row in cursor2:
-    twitacct = row[0]
-    dbid = row[1]
+myfile = open('foamers.csv', 'wb')
+wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
-    print twitacct
-    print dbid
-    #wr.writerow(twithandle, started)
+c.execute("SELECT twithandle, started FROM foamites LIMIT 10") ##select 100 people who haven't yet been scraped
+for row in c:
+    twithandle = row[0]
+    started = row[1]
+
+    print twithandle
+    print started
+    wr.writerow(twithandle, started)
