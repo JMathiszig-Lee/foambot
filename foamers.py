fb93c764f1d106df21c1d063b3a65b42716dc9c7
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 13:15:46 +0100

    CSV time

--- foamers.py
+++ foamers.py
@@ -1,18 +1,14 @@
 import sqlite3
+import csv
 
 conn = sqlite3.connect('Scraper/foambot.sqlite')
 c = conn.cursor()
 
-c.execute("SELECT COUNT(*) FROM foamites")
-result = c.fetchone()
-number_of_rows = result[0]
-print (result)
-print number_of_rows
+myfile = open(foamers.csv, 'wb')
+wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
-c.execute("SELECT * FROM foamites WHERE started=0 ")
-not_started = c.rowcount
-print not_started
-
-c.execute("SELECT * FROM foamites WHERE started=1 ")
-started = c.rowcount
-print started
+c.execute("SELECT twithandle, started FROM foamites ") ##select 100 people who haven't yet been scraped
+for row in c:
+    twithandle = row[0]
+    started = row[1]
+    wr.writerow(twithandle, started)
