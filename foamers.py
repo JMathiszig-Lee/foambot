6d853e1b85d58e57ff10bbb4d6f70ddeb8274c07
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 11:43:08 +0100

    sorting out stuff

--- foamers.py
+++ foamers.py
@@ -3,11 +3,16 @@ import sqlite3
 conn = sqlite3.connect('Scraper/foambot.sqlite')
 c = conn.cursor()
 
-c.execute("SELECT COUNT(*), * FROM foamites")
+c.execute("SELECT COUNT(*) FROM foamites")
 result = c.fetchone()
-
 number_of_rows = result[0]
-
-print (c.fetchall())
-
+print (c.fetchall)
 print number_of_rows
+
+c.execute("SELECT * FROM foamites WHERE started = 0 ")
+not_started = c.rowsaffected
+print not_started
+
+c.execute("SELECT * FROM foamites WHERE started = 1 ")
+started = c.rowsaffected
+print started
