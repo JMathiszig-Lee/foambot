70d7e28370629e528d0f4d4a6251129e84c02aab
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 13:17:53 +0100

    sr

--- foamers.py
+++ foamers.py
@@ -4,7 +4,7 @@ import csv
 conn = sqlite3.connect('Scraper/foambot.sqlite')
 c = conn.cursor()
 
-myfile = open(foamers.csv, 'wb')
+myfile = open('foamers.csv', 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
 c.execute("SELECT twithandle, started FROM foamites ") ##select 100 people who haven't yet been scraped
