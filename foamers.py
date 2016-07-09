3b41853845f71bc4d0afa6f8741605ed042aaa36
Jakob Mathiszig-Lee <Jakob@mathisziglee.co.uk>
2016-07-09 13:43:29 +0100

    maybe it will work this time?

--- foamers.py
+++ foamers.py
@@ -7,6 +7,10 @@ c = conn.cursor()
 myfile = open('foamers.csv', 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
+c.execute("SELECT COUNT(*) FROM foamites")
+number_of_rows=c.fetchone()
+print number_of_rows
+
 c.execute("SELECT twithandle, started FROM foamites LIMIT 10") ##select 100 people who haven't yet been scraped
 for row in c:
     twithandle = row[0]
@@ -14,4 +18,5 @@ for row in c:
 
     print twithandle
     print started
-    wr.writerow(twithandle, started)
+    data = [twithandle, started]
+    wr.writerow(data)
